6a3d75a0dd7856aed231bd98b720a0f421937114
SANKALP KASHYAP <sankalp112kashyap@gmail.com>
2025-03-16 04:12:38 -0700

    added debloat inventory-host

--- lib/ansible/inventory/host.py
+++ lib/ansible/inventory/host.py
@@ -31,138 +31,100 @@ class Host:
     """ a single ansible host """
     base_type = InventoryObjectType.HOST
 
-    # __slots__ = [ 'name', 'vars', 'groups' ]
-
-    def __getstate__(self):
-        return self.serialize()
-
-    def __setstate__(self, data):
-        return self.deserialize(data)
+    def __init__(self, name=None, port=None, gen_uuid=True):
+        self.name = name
+        self.address = name
+        self.vars = {}
+        self.groups = []
+        self._uuid = get_unique_id() if gen_uuid else None
+        self.implicit = False
+        
+        if port:
+            self.set_variable('ansible_port', int(port))
 
     def __eq__(self, other):
-        if not isinstance(other, Host):
-            return False
-        return self._uuid == other._uuid
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
+        return isinstance(other, Host) and self._uuid == other._uuid
 
     def __hash__(self):
         return hash(self.name)
 
     def __str__(self):
-        return self.get_name()
-
-    def __repr__(self):
-        return self.get_name()
-
-    def serialize(self):
-        groups = []
-        for group in self.groups:
-            groups.append(group.serialize())
-
-        return dict(
-            name=self.name,
-            vars=self.vars.copy(),
-            address=self.address,
-            uuid=self._uuid,
-            groups=groups,
-            implicit=self.implicit,
-        )
-
-    def deserialize(self, data):
-        self.__init__(gen_uuid=False)  # used by __setstate__ to deserialize in place  # pylint: disable=unnecessary-dunder-call
-
-        self.name = data.get('name')
-        self.vars = data.get('vars', dict())
-        self.address = data.get('address', '')
-        self._uuid = data.get('uuid', None)
-        self.implicit = data.get('implicit', False)
-
-        groups = data.get('groups', [])
-        for group_data in groups:
-            g = Group()
-            g.deserialize(group_data)
-            self.groups.append(g)
-
-    def __init__(self, name=None, port=None, gen_uuid=True):
-
-        self.vars = {}
-        self.groups = []
-        self._uuid = None
-
-        self.name = name
-        self.address = name
-
-        if port:
-            self.set_variable('ansible_port', int(port))
-
-        if gen_uuid:
-            self._uuid = get_unique_id()
-        self.implicit = False
-
-    def get_name(self):
         return self.name
 
+    __repr__ = __str__
+
     def populate_ancestors(self, additions=None):
-        # populate ancestors
-        if additions is None:
-            for group in self.groups:
-                self.add_group(group)
+        if additions:
+            self.groups.extend(g for g in additions if g not in self.groups)
         else:
-            for group in additions:
-                if group not in self.groups:
-                    self.groups.append(group)
+            for group in self.groups:
+                ancestors = set(group.get_ancestors()) - set(self.groups)
+                self.groups.extend(ancestors)
 
     def add_group(self, group):
-        added = False
-        # populate ancestors first
-        for oldg in group.get_ancestors():
-            if oldg not in self.groups:
-                self.groups.append(oldg)
-
-        # actually add group
+        ancestors = set(group.get_ancestors()) - set(self.groups)
+        self.groups.extend(ancestors)
         if group not in self.groups:
             self.groups.append(group)
-            added = True
-        return added
+            return True
+        return False
 
     def remove_group(self, group):
-        removed = False
-        if group in self.groups:
-            self.groups.remove(group)
-            removed = True
-
-            # remove exclusive ancestors, xcept all!
-            for oldg in group.get_ancestors():
-                if oldg.name != 'all':
-                    for childg in self.groups:
-                        if oldg in childg.get_ancestors():
-                            break
-                    else:
-                        self.remove_group(oldg)
-        return removed
+        if group not in self.groups:
+            return False
+            
+        self.groups.remove(group)
+        
+        # Remove exclusive ancestors except 'all'
+        ancestors = set(group.get_ancestors())
+        for ancestor in ancestors:
+            if ancestor.name == 'all':
+                continue
+            if not any(ancestor in g.get_ancestors() for g in self.groups):
+                self.remove_group(ancestor)
+        return True
 
     def set_variable(self, key, value):
-        if key in self.vars and isinstance(self.vars[key], MutableMapping) and isinstance(value, Mapping):
+        if isinstance(self.vars.get(key), MutableMapping) and isinstance(value, Mapping):
             self.vars = combine_vars(self.vars, {key: value})
         else:
             self.vars[key] = value
 
-    def get_groups(self):
-        return self.groups
-
     def get_magic_vars(self):
-        results = {}
-        results['inventory_hostname'] = self.name
-        if patterns['ipv4'].match(self.name) or patterns['ipv6'].match(self.name):
-            results['inventory_hostname_short'] = self.name
-        else:
-            results['inventory_hostname_short'] = self.name.split('.')[0]
-
-        results['group_names'] = sorted([g.name for g in self.get_groups() if g.name != 'all'])
-
-        return results
+        hostname_short = self.name if patterns['ipv4'].match(self.name) or patterns['ipv6'].match(self.name) else self.name.split('.')[0]
+        
+        return {
+            'inventory_hostname': self.name,
+            'inventory_hostname_short': hostname_short,
+            'group_names': sorted(g.name for g in self.groups if g.name != 'all')
+        }
 
     def get_vars(self):
         return combine_vars(self.vars, self.get_magic_vars())
+
+    # Maintain serialization for compatibility
+    def serialize(self):
+        return {
+            'name': self.name,
+            'vars': self.vars.copy(),
+            'address': self.address,
+            'uuid': self._uuid,
+            'groups': [g.serialize() for g in self.groups],
+            'implicit': self.implicit,
+        }
+
+    def deserialize(self, data):
+        self.__init__(gen_uuid=False)
+        self.name = data.get('name')
+        self.vars = data.get('vars', {})
+        self.address = data.get('address', '')
+        self._uuid = data.get('uuid')
+        self.implicit = data.get('implicit', False)
+        
+        for group_data in data.get('groups', []):
+            g = Group()
+            g.deserialize(group_data)
+            self.groups.append(g)
+
+    __getstate__ = serialize
+    __setstate__ = deserialize
